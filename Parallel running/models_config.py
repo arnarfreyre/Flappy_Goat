487427999c00d16981e5fb812787423ef51a84b1
arnarfreyrerlingsson <arnarfreyre@gmail,com>
2026-03-04 16:36:16 +0000

    Klara og runna

--- Parallel running/models_config.py
+++ Parallel running/models_config.py
@@ -5,12 +5,18 @@ l1 = [128,128,128]
 l2 = [256,256,256]
 l3 = [512,512,512]
 l4 = [1024,1024,1024]
-#l5 = [2048,2048,2048]
-#l6 = [4096,4096,4096]
+l5 = [2048,2048,2048]
+l6 = [4096,4096,4096]
 
 
-models = [l4, l0, l1,l2,l3]
+models = [l6,l0,l5,l1,l4,l2,l3]
 
-model_names = [
-    "NN1024x3","NN64x3","NN128x3","NN256x3","NN512x3"
-]
+model_names = []
+for model in models:
+    length = len(model)
+    name = model[0]
+    name_str = "NN" + str(name) + "x"+str(length)
+
+    model_names.append(name_str)
+
+#model_names = ['NN4096x3', 'NN64x3', 'NN2048x3', 'NN128x3', 'NN1024x3', 'NN256x3', 'NN512x3']
