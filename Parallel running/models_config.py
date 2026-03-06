095da16e19334b66b643d791a9ff047f0d909a18
arnarfreyrerlingsson <arnarfreyre@gmail,com>
2026-03-06 20:39:36 +0000

    Klara og runna

--- Parallel running/models_config.py
+++ Parallel running/models_config.py
@@ -1,24 +1,5 @@
+# Model sizes from 32x3 to 8192x3
+sizes = [32, 48, 64, 96, 128, 192, 256, 384, 512, 768, 1024, 1536, 2048, 3072, 4096, 6144, 8192]
 
-
-l0 = [32,32,32]
-l1 = [50,50,50]
-l2 = [100,100,100]
-l3 = [200,200,200]
-l4 = [400,400,400]
-l5 = [800,800,800]
-l6 = [256,256,256]
-l7 = [80,80,80]
-l8 = [300,300,300]
-
-
-models = [l6,l2,l6,l2,l7,l7,l8]
-
-model_names = []
-for model in models:
-    length = len(model)
-    name = model[0]
-    name_str = "NN" + str(name) + "x"+str(length)
-
-    model_names.append(name_str)
-
-#model_names = ['NN4096x3', 'NN64x3', 'NN2048x3', 'NN128x3', 'NN1024x3', 'NN256x3', 'NN512x3']
+models = [[s, s, s] for s in sizes]
+model_names = [f"NN{s}x3" for s in sizes]
